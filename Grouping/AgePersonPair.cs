544cdbabe59547c22f87ff1cfd4b37f3594feac8
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-05-05 13:47:17 +0200

    Updated grouping sample

---
+++ Grouping/AgePersonPair.cs
@@ -0,0 +1,57 @@
+﻿namespace Grouping
+{
+    using System;
+    using System.Collections.Generic;
+    using DynamicData;
+    using DynamicData.Binding;
+
+    public class AgePersonPair : AbstractNotifyPropertyChanged, IDisposable
+    {
+        private readonly IGroup<Person, int, int> g;
+        private IEnumerable<Person> people;
+        private readonly IDisposable cleanup;
+        private PersonGroupViewModel personGroup;
+
+        public AgePersonPair(IGroup<Person, int, int> group)
+        {
+            cleanup = group.Cache.Connect()
+                .QueryWhenChanged(
+                    query =>
+                    {
+                        var count = query.Count;
+                        var items = query.Items;
+                        return new PersonGroupViewModel(count, items);
+                    })
+                .Subscribe(model => PersonGroup = model);
+
+            Age = group.Key;
+        }
+
+        public PersonGroupViewModel PersonGroup
+        {
+            get { return personGroup; }
+            set { SetAndRaise(ref personGroup, value); }
+        }    
+
+        public int Age { get; set; }
+
+        public int PeopleCount { get; set; }
+
+        public void Dispose()
+        {
+            cleanup.Dispose();
+        }
+    }
+
+    public class PersonGroupViewModel
+    {
+        public int Count { get; set; }
+        public IEnumerable<Person> People { get; set; }
+
+        public PersonGroupViewModel(int count, IEnumerable<Person> people)
+        {
+            Count = count;
+            People = people;
+        }
+    }
+}
---
+++ Grouping/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿namespace Grouping
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Reactive.Linq;
+    using DynamicData;
+
+    public class MainViewModel
+    {
+        private readonly ReadOnlyObservableCollection<AgePersonPair> groupedByAgeCollection;
+        private readonly ReadOnlyObservableCollection<Person> peopleCollection;
+
+        public MainViewModel()
+        {
+            var peopleObs = CreatePeopleObservable().Publish();
+
+            var observableChangeSet = peopleObs.ToObservableChangeSet();
+
+            var groupChangeSet = observableChangeSet
+                .Group(person => person.Age)
+                .Transform((group, i) => new AgePersonPair(group));
+
+            groupChangeSet
+                .ObserveOnDispatcher()
+                .Bind(out groupedByAgeCollection)
+                .Subscribe();
+
+            var peopleChangeSet = observableChangeSet;
+
+            peopleChangeSet
+                .ObserveOnDispatcher()
+                .Bind(out peopleCollection)
+                .Subscribe();
+
+            peopleObs.Connect();
+        }
+
+        public ReadOnlyObservableCollection<AgePersonPair> GroupedByAgeCollection => groupedByAgeCollection;
+
+        public ReadOnlyObservableCollection<Person> People => peopleCollection;
+
+        private IObservable<Person> CreatePeopleObservable()
+        {
+            var people = new[]
+            {
+                new Person("JMN", 1),
+                new Person("Andrea", 1),
+                new Person("Ana", 1),
+                new Person("Pepito", 2),
+                new Person("Johnny", 4),
+                new Person("Mary", 1),
+                new Person("Rose", 4),
+                new Person("Anthony", 3),
+                new Person("David", 2),
+                new Person("Joanna", 5),
+                new Person("Oscar", 4)
+            };
+
+            var intervalObs = Observable.Interval(TimeSpan.FromSeconds(2));
+            var peopleObs = people.ToObservable();
+            var intervalPeopleObs = intervalObs.Zip(peopleObs, (_, person) => person);
+            return intervalPeopleObs;
+        }
+    }
+}
