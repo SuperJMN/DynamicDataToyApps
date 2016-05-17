1f14582ee8846ef1a8038e09bdeea2b9385f7330
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-05-17 16:33:51 +0200

    Added Switch sample

---
+++ Switch/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿namespace ReactiveLocura
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+    using System.Text;
+    using Cinch;
+    using DynamicData;
+    using ReactiveUI;
+
+    public class MainViewModel
+    {
+        private readonly IOpenFileService openFileService;
+        private readonly ISubject<IObservable<string>> workUnits = new Subject<IObservable<string>>();
+        private readonly ReadOnlyObservableCollection<string> lines;
+        private readonly IConnectableObservable<string> observableLines;
+
+        public MainViewModel(IOpenFileService openFileService)
+        {
+            this.openFileService = openFileService;
+            OpenFileCommand = ReactiveCommand.Create();
+            OpenFileCommand.Subscribe(_ => OpenFromFile());
+
+            observableLines = workUnits                
+                .Switch()
+                .Publish();
+            
+            observableLines
+                .ToObservableChangeSet()
+                .ObserveOnDispatcher()
+                .Bind(out lines)
+                .Subscribe();
+        }
+
+        public IEnumerable<string> Lines => lines;
+
+        private void OpenFromFile()
+        {
+            var dialogResult = openFileService.ShowDialog(null);
+            if (dialogResult == true)
+            {
+                var path = openFileService.FileName;
+                observableLines.Connect();
+
+                var linesFromFile = ToLines(new StreamReader(path, Encoding.Default));
+
+                var observableLinesFromFile = linesFromFile
+                    .ToObservable()
+                    .PushEvery(TimeSpan.FromSeconds(1));
+
+                workUnits.OnNext(observableLinesFromFile);
+            }
+        }
+
+        private static IEnumerable<string> ToLines(StreamReader streamReader)
+        {
+            while (!streamReader.EndOfStream)
+            {
+                yield return streamReader.ReadLine();
+            }
+        }
+
+        public ReactiveCommand<object> OpenFileCommand { get; }        
+    }
+}
